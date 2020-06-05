224d441b50e62990156fe576273e12b851038c53
Nabreet <nabby.ghumman@gmail.com>
2020-06-05 04:04:39 -0700

    Initial Commit

---
+++ A_Algorithm/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  A_Algorithm
+//
+//  Created by Nabreet Ghumman on 2020-06-05.
+//  Copyright © 2020 Nabreet Ghumman. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
